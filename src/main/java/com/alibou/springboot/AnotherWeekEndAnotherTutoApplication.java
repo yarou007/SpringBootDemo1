a95c8d5be72f169761b9c4fdfe644cf6f76d5ddd
Yacine Hammi <hammi.yacine.1@gmail.com>
2023-07-28 20:32:47 +0200

    intializing spring boot project on github

---
+++ src/main/java/com/alibou/springboot/AnotherWeekEndAnotherTutoApplication.java
@@ -0,0 +1,13 @@
+package com.alibou.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherWeekEndAnotherTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotherWeekEndAnotherTutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alibou/springboot/AnotherWeekEndAnotherTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotherWeekEndAnotherTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
